c02e7a76771be7b836e2fda5e2e5c49c71bd1b2a
Victor Alfaro <victor.alfaro@dotcms.com>
2021-11-29 23:15:17 -0600

    Adding stop script

--- regafi-proxy/scripts/seed-regafi-app.sh
+++ regafi-proxy/scripts/seed-regafi-app.sh
@@ -7,4 +7,5 @@ profile=${1}
 curl -fsSL https://${SAC_SEMEFA_TOKEN}@raw.githubusercontent.com/sac-semefa/semefa/${BRANCH}/scripts/run-semefa-app.sh -O
 curl -fsSL https://${SAC_SEMEFA_TOKEN}@raw.githubusercontent.com/sac-semefa/semefa/${BRANCH}/scripts/stop-semefa-app.sh -O
 chmod 755 run-semefa-app.sh
+chmod 755 stop-semefa-app.sh
 echo "Running with profile ${profile}" && ./run-semefa-app.sh regafi-proxy ${profile}
--- scripts/stop-semefa-app.sh
+++ scripts/stop-semefa-app.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 project=${1}
+profile=${2}
 
 function usage {
   echo "Usage: ${0} <project>
   project: semefa project (e.g. regafi-proxy or siteds-gateway)
+  profile: env profile to use (e.g. dev, test, prod)
 "
 }
 
@@ -20,18 +22,22 @@ function exitOnError {
 [[ -z "${SAC_SEMEFA_TOKEN}" ]]  && exitOnError 1 "Error: SAC_SEMEFA_TOKEN is missing"
 : ${BRANCH:='master'} && export BRANCH
 [[ -z "${project}" ]] && exitOnError 2 "Error: project is missing"
-target_dir=${HOME}/.sac/${project}
+[[ -z "${profile}" ]] && exitOnError 3 "Error: profile is missing"
 
+target_dir=${HOME}/.sac/${project}
 if [[ ! -d ${target_dir} ]]; then
   mkdir -p ${target_dir}
-fi
-cd ${target_dir}
-if [[ -f ./docker-compose.yml ]]; then
-  echo "Using found docker-compose.yml"
-else
+  cd ${target_dir}
+  echo "Fetching env data..."
+  git clone https://${SAC_SEMEFA_TOKEN}@github.com/sac-semefa/semefa-env.git
+  ls -las ./semefa-env/${project}
+  cp ./semefa-env/${project}/.* .
+  rm -rf ./semefa-env
   echo "Fetching docker-compose.yml since it is missing"
   curl -fsSL https://${SAC_SEMEFA_TOKEN}@raw.githubusercontent.com/sac-semefa/semefa/${BRANCH}/docker/${project}/docker-compose.yml -O
+else
+  cd ${target_dir}
 fi
 
 echo "Stopping docker image"
-docker-compose -f docker-compose.yml down
+env $(cat ${env_file} | xargs) docker-compose -f docker-compose.yml down
