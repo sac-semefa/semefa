ba5fdc195e6a704ea370d9e0c9316da2daafe1ea
Victor Alfaro <victor.alfaro@dotcms.com>
2022-03-07 12:53:05 -0600

    Revert "Removing jdbc"
    This reverts commit 84c58cb40d3dc5e8629911817fd911e49455fd67.

--- siteds-gateway/src/main/kotlin/com/saludaunclic/semefa/siteds/SitedsGatewayApp.kt
+++ siteds-gateway/src/main/kotlin/com/saludaunclic/semefa/siteds/SitedsGatewayApp.kt
@@ -3,8 +3,10 @@ package com.saludaunclic.semefa.siteds
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
 
 @SpringBootApplication(scanBasePackages = [ "com.saludaunclic.semefa.common", "com.saludaunclic.semefa.siteds" ])
+@EnableJdbcRepositories(basePackages = [ "com.saludaunclic.semefa.common.repository" ])
 @EnableCaching
 class SitedsGatewayApp
 
