8213556f0af769040b5c164c73a9ba985abf02d0
Victor Alfaro <victor.alfaro@dotcms.com>
2022-11-29 18:56:49 -0600

    update of stop semefa app

--- scripts/stop-semefa-app.sh
+++ scripts/stop-semefa-app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 project=${1}
+profile=${2}
 
 sac_home=${HOME}/.sac
 source ${sac_home}/semefa-lib.sh
@@ -8,14 +9,16 @@ source ${sac_home}/semefa-lib.sh
 function usage {
   echo "Usage: ${0} <project> <profile>
     project: (required) semefa project (e.g. regafi-proxy or siteds-gateway)
+    profile: (required) env profile to use (e.g. dev, test, prod)
 "
 }
 
 cd ${sac_home}
+processProfile
 showInfo
 
 echo "Checking for ${project} container running" \
   && container_id=$(docker ps -q -f name=${project})
 [[ -n "${container_id}" ]] \
   && echo "Found ${project} container: ${container_id}, stopping it" \
-  && docker-compose -f ./${project}/docker-compose.yml down
+  && env $(cat ${env_file} | xargs) docker-compose -f ./${project}/docker-compose.yml down
